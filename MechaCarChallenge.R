abcdc49a65ddd9af0d8e2091acd99b1bb47f0cbc
jaggarwal20 <80078651+jaggarwal20@users.noreply.github.com>
2021-06-12 13:04:48 -0500

    Updated with Deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -2,4 +2,7 @@ library(dplyr)
 mechacar_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import dataset
 #Linear Regression
 reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD,data=mechacar_table)
-summary(reg)
+mechacar_summary <- summary(reg)
+suspension<- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F) #import dataset
+total_summary <- suspension %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Num_Vehicles=n(),SD =sd(PSI), .groups = 'keep') #create summary table with multiple columns
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Num_Vehicles=n(),SD =sd(PSI), .groups = 'keep') #summary table for manufacturing lot
