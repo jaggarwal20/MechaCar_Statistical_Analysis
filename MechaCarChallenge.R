ca48fbab48f3915f5cdf9f9c4aec7957bbca17e8
jaggarwal20 <80078651+jaggarwal20@users.noreply.github.com>
2021-06-20 09:00:56 -0500

    Updated scripts

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -5,7 +5,7 @@ library(dplyr)
 mechacar_table <- read.csv('MechaCar_mpg.csv',check.names = F,stringsAsFactors = F) #import dataset
 
 #Linear Regression
-reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD,data=mechacar_table)
+reg <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + AWD + ground_clearance,data=mechacar_table)
 
 #create a summary to identify the p value and r-squared value
 mechacar_summary <- summary(reg)
@@ -17,7 +17,7 @@ suspension<- read.csv('Suspension_Coil.csv',check.names = F,stringsAsFactors = F
 total_summary <- suspension %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Num_Vehicles=n(),SD =sd(PSI), .groups = 'keep') #create summary table with multiple columns
 
 #create a lot summary
-lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Num_Vehicles=n(),SD =sd(PSI), .groups = 'keep') #summary table for manufacturing lot
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),Median_PSI=median(PSI),Variance=var(PSI),SD =sd(PSI), .groups = 'keep') #summary table for manufacturing lot
 
 #perform t-test on suspension data
 t.test(suspension$PSI, mu=1500)
